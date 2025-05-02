a436c0a54a0ec8cfd7182380627f82125a3c2a40
qqoro <arcaqoro@gmail.com>
2025-05-02 19:19:55 +0900

    feat: RJ코드 조회방식 변경
    - html 크롤링에서 API로 변경

--- src/main/handlers/thumbnail.ts
+++ src/main/handlers/thumbnail.ts
@@ -236,7 +236,103 @@ async function getFromGoogle({ page, query }: { page: Page; query: string }) {
   }
 }
 
+interface DLSiteProductInfo {
+  site_id: string;
+  maker_id: string;
+  age_category: number;
+  affiliate_deny: number;
+  dl_count: number;
+  wishlist_count: number;
+  dl_format: number;
+  rate_average: number;
+  rate_average_2dp: number;
+  rate_average_star: number;
+  rate_count: number;
+  review_count: number;
+  price: number;
+  price_without_tax: number;
+  price_str: string;
+  default_point_rate: number;
+  default_point: number;
+  product_point_rate: any;
+  dlsiteplay_work: boolean;
+  is_ana: boolean;
+  is_sale: boolean;
+  on_sale: number;
+  is_discount: boolean;
+  is_pointup: boolean;
+  gift: any[];
+  is_rental: boolean;
+  work_rentals: any[];
+  upgrade_min_price: number;
+  down_url: string;
+  is_tartget: any;
+  title_id: any;
+  title_name: any;
+  title_name_masked: any;
+  title_volumn: any;
+  title_work_count: any;
+  is_title_completed: boolean;
+  bulkbuy_key: any;
+  bonuses: any[];
+  is_limit_work: boolean;
+  is_sold_out: boolean;
+  limit_stock: number;
+  is_reserve_work: boolean;
+  is_reservable: boolean;
+  is_timesale: boolean;
+  timesale_stock: number;
+  is_free: boolean;
+  is_oly: boolean;
+  is_led: boolean;
+  is_noreduction: boolean;
+  is_wcc: boolean;
+  work_name: string;
+  work_name_masked: string;
+  work_image: string;
+  sales_end_info: any;
+  voice_pack: any;
+  regist_date: string;
+  work_type: string;
+  book_type: any;
+  discount_calc_type: any;
+  is_pack_work: boolean;
+  limited_free_terms: any[];
+  official_price: number;
+  options: string;
+  custom_genres: string[];
+  dl_count_total: number;
+  dl_count_items: any[];
+  default_point_str: string;
+}
+
 async function getFromDLSite({ page, rjCode }: { page: Page; rjCode: string }) {
+  rjCode = rjCode.toUpperCase();
+  const data = (await fetch(
+    `https://www.dlsite.com/maniax/product/info/ajax?product_id=${rjCode}&cdn_cache_min=1`,
+    {
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        priority: "u=1, i",
+        "sec-ch-ua": '"Chromium";v="134", "Whale";v="4", "Not.A/Brand";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        Referer: `https://www.dlsite.com/maniax/work/=/product_id/${rjCode}.html`,
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+      },
+      body: null,
+      method: "GET",
+    }
+  ).then((res) => res.json())) as Record<typeof rjCode, DLSiteProductInfo>;
+  if (data?.[rjCode]?.work_image) {
+    return "https:" + data[rjCode].work_image;
+  }
+
   await page.goto(
     `https://www.dlsite.com/maniax/work/=/product_id/${rjCode}.html`
   );
