0d67e5c4ff2fc6087f672c0c12d255daeca14f7f
pilgeun48 <pilgeun48@gmail.com>
2025-10-12 09:04:30 +0900

    feat: 겟츄 탐색 프리픽스에 `GETCHU` 추가

--- src/main/collectors/getchu-collector.ts
+++ src/main/collectors/getchu-collector.ts
@@ -6,7 +6,7 @@ import { Collector } from "./registry.js";
 export const GetchuCollector: Collector = {
   name: "Getchu",
   getId: async (path) => {
-    const id = /GC\d{1,7}/i.exec(path)?.[0].slice(2);
+    const [, id] = /(?:GC|GETCHU)(\d{1,7})/i.exec(path) ?? [];
     return id;
   },
   fetchInfo: async ({ id }) => {
